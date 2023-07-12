b3e131cb12db097fd50b47e7503ce40e33373432
mrprotocoll <lekanvgbg@gmail.com>
2023-07-12 14:29:46 +0100

    Add column

--- db/migrate/20230712130321_add_column_password_digest_to_users.rb
+++ db/migrate/20230712130321_add_column_password_digest_to_users.rb
@@ -1,5 +1,5 @@
 class AddColumnPasswordDigestToUsers < ActiveRecord::Migration[7.0]
   def change
-    change_column :users, :password_digest, :string
+    add_column :users, :password_digest, :string
   end
 end
